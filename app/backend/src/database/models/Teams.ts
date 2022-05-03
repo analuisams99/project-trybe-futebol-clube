09a48e672e7f8945f88a4776b210cb935764eed1
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-05-03 19:34:38 -0300

    Cria migration e model Teams

---
+++ app/backend/src/database/models/Teams.ts
@@ -0,0 +1,28 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  public id: number;
+
+  public teamName: string;
+}
+
+Teams.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  team_name: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Teams;
