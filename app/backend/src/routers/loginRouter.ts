aa2c0e5b05517182b101213610952a50e700c63d
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-05-10 16:26:59 -0300

    Cria rota para validação de login

--- app/backend/src/routers/loginRouter.ts
+++ app/backend/src/routers/loginRouter.ts
@@ -4,10 +4,11 @@ import LoginController from '../controllers/login';
 
 const router = Router();
 
-const { login } = new LoginController();
+const { login, loginValidate } = new LoginController();
 const { emailValidation, passValidation } = UserValidation;
 
 router
-  .post('/', emailValidation, passValidation, login);
+  .post('/', emailValidation, passValidation, login)
+  .get('/validate', loginValidate);
 
 export default router;
