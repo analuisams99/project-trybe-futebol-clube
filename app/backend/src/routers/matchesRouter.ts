90bdc7557e13bb772f081b4692d2bcd3cf8f8fc9
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-05-11 15:28:27 -0300

    Cria rota para criar e atualizar Matches

--- app/backend/src/routers/matchesRouter.ts
+++ app/backend/src/routers/matchesRouter.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import TokenValidation from '../middlewares/tokenValidation';
 import MatchesController from '../controllers/matches';
 
 const router = Router();
 
-const { getAll } = new MatchesController();
+const { getAll, create, inProgressUpdate } = new MatchesController();
+const { tokenValidation } = new TokenValidation();
 
 router
+  .post('/', tokenValidation, create)
+  .patch('/:id/finish', inProgressUpdate)
   .get('/', getAll);
 
 export default router;
