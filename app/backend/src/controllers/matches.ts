e21a0fa43514ddc2d09ce5aef458203c62873fa4
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-05-11 15:35:31 -0300

    Controller de criação de Matches com filtro

--- app/backend/src/controllers/matches.ts
+++ app/backend/src/controllers/matches.ts
@@ -2,12 +2,48 @@ import { NextFunction, Request, Response } from 'express';
 import MatchesService from '../services/matches';
 import statusCode from '../utils/statusCode';
 
-const { OK, BadRequest } = statusCode.StatusCodes;
+const { OK, CREATED } = statusCode.StatusCodes;
+const {
+  equalTeams, notFoundTeamById, matchFinished, matchesNotFound,
+} = statusCode.errors;
 
 export default class MatchesController {
   constructor(
     private _matchesService = new MatchesService(),
   ) { }
+
+  public create = async (req: Request, res: Response, next: NextFunction)
+  : Promise<void | Response> => {
+    try {
+      const { homeTeam, homeTeamGoals, awayTeam, awayTeamGoals, inProgress } = req.body;
+      if (homeTeam === awayTeam) {
+        return res.status(equalTeams.status).json({ message: equalTeams.message });
+      }
+
+      const MatchCreated = await this._matchesService.create(
+        { homeTeam, homeTeamGoals, awayTeam, awayTeamGoals, inProgress },
+      );
+
+      if (MatchCreated === false || !MatchCreated) {
+        return res.status(notFoundTeamById.status).json({ message: notFoundTeamById.message });
+      }
+
+      return res.status(CREATED).json(MatchCreated);
+    } catch (e) {
+      next(e);
+    }
+  };
+
+  public inProgressUpdate = async (req: Request, res: Response, next: NextFunction)
+  : Promise<void> => {
+    try {
+      const { id } = req.params;
+      await this._matchesService.inProgressUpdate(+id);
+      res.status(matchFinished.status).json(matchFinished.message);
+    } catch (e) {
+      next(e);
+    }
+  };
 
   public getAll = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
@@ -15,7 +51,7 @@ export default class MatchesController {
 
       const matches = await this._matchesService.getAll(inProgress as string | undefined);
       if (!matches) {
-        res.status(BadRequest).json({ message: 'Matches not found' });
+        res.status(matchesNotFound.status).json(matchesNotFound.message);
       }
       res.status(OK).json(matches);
     } catch (e) {
