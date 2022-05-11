974d1e498df122aff4df1e840c0eebc456f2c899
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-05-10 23:06:30 -0300

    interface de Matches

---
+++ app/backend/src/interfaces/Matches.ts
@@ -0,0 +1,23 @@
+export interface MatchEntity {
+  id: number;
+  homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: boolean;
+}
+
+export interface MatchWithTeams {
+  id: number;
+  homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: boolean;
+  teamHome: {
+    teamName: string
+  },
+  teamAway: {
+    teamName: string
+  }
+}
