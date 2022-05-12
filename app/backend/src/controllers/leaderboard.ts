35e3790268ee24def9b016779e8796a33ee06780
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-05-12 20:00:50 -0300

    Controller de Leaderboard, cria função para pegar todos os leaderboards finalizados

---
+++ app/backend/src/controllers/leaderboard.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express';
+import LeaderboardService from '../services/leaderboard';
+import statusCode from '../utils/statusCode';
+
+const { OK } = statusCode.StatusCodes;
+
+export default class LeaderboardController {
+  constructor(
+    private _leaderboardService = new LeaderboardService(),
+  ) { }
+
+  public getAllLeaderboard = async (req: Request, res: Response, next: NextFunction)
+  : Promise<void> => {
+    try {
+      const allLeaderboards = await this._leaderboardService.getLeaderboard();
+      res.status(OK).json(allLeaderboards);
+    } catch (e) {
+      next(e);
+    }
+  };
+}
+// homeTeamLeaderboards, getAllHomeLeaderboard
